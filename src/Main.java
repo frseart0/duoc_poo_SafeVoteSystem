75d66bc681e1da3d40763b658315c1ed65a1f8b9
frsearto <frsearto@gmail.com>
2025-07-10 16:10:31 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import modelo.PrimeList;
+
+public class Main {
+    public static void main(String[] args) {
+        PrimeList lista = new PrimeList();
+        lista.add(7);
+        lista.add(11);
+        System.out.println(lista);
+    }
+}
---
+++ src/modelo/PrimeList.java
@@ -0,0 +1,29 @@
+package modelo;
+import java.util.ArrayList;
+
+public class PrimeList extends ArrayList<Integer>{
+
+    public boolean isPrime(int numero){
+        if (numero <= 1) {
+            return false;
+        }
+        if (numero == 2) {
+            return true;
+        }
+        if (numero % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(numero); i += 2) {
+            if (numero % i == 0) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean add(Integer numero) {
+        if (!isPrime(numero)) {
+            throw new IllegalArgumentException("numero invalido");
+        }
+        return super.add(numero);
+    }
+}
